542fbde3b201bdbd73bedfd8f381896d8bc2d312
hrg2682 <60620703+hrg2682@users.noreply.github.com>
2023-11-20 10:10:03 +0500

    Added Dot net components

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,24 +1,43 @@
 ﻿using System;
-using ProjectA.TeamA;
+using System.Collections.Generic;
 
 class Program
 {
     static void Main()
     {
-     
-    }
-}
+        // Create a List of strings
+        List<string> stringList = new List<string>();
 
-namespace ProjectA
-{
-    namespace TeamA
-    {
-        class ClassB
+        // Add elements to the list
+        stringList.Add("Item A");
+        stringList.Add("Item B");
+        stringList.Add("Item C");
+
+        // Access elements by index
+        Console.WriteLine("List elements:");
+        for (int i = 0; i < stringList.Count; i++)
         {
-            public static void Print()
-            {
-                Console.WriteLine("Team B print method");
-            }
+            Console.WriteLine($"Item {i + 1}: {stringList[i]}");
+        }
+
+        // Remove an element
+        //stringList.Remove("Item B");
+
+        // Check if an element exists
+        if (stringList.Contains("Item B"))
+        {
+            Console.WriteLine("Item B is in the list.");
+        }
+        else
+        {
+            Console.WriteLine("Item B is not in the list.");
+        }
+
+        // Display the updated list
+        Console.WriteLine("\nUpdated list elements:");
+        foreach (var item in stringList)
+        {
+            Console.WriteLine(item);
         }
     }
 }
--- ConsoleApp1/obj/Debug/net6.0/ConsoleApp1.AssemblyInfo.cs
+++ ConsoleApp1/obj/Debug/net6.0/ConsoleApp1.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a3ee447294fc079525ca234c703d082a387554af")]
 [assembly: System.Reflection.AssemblyProductAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
